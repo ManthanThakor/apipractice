e76dee282ee716c61c1474d4eb705cbcae0427c6
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-14 19:43:58 +0530

    Music section 9

---
+++ _010music/script.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const audio = document.getElementById('audio');
+//     const playPauseButton = document.getElementById('playPause');
+//     const stopButton = document.getElementById('stop');
+  
+//     playPauseButton.addEventListener('click', function () {
+//       if (audio.paused) {
+//         audio.play();
+//         playPauseButton.textContent = 'Pause';
+//       } else {
+//         audio.pause();
+//         playPauseButton.textContent = 'Play';
+//       }
+//     });
+  
+//     stopButton.addEventListener('click', function () {
+//       audio.pause();
+//       audio.currentTime = 0;
+//       playPauseButton.textContent = 'Play';
+//     });
+//   });
+  
